d67eefc92b54a650d01eae2914734c22454a2d59
martin_mwiti <martin@ariyafinergy.com>
2023-03-12 17:32:26 +0300

    Minor change

--- Interface/interface.py
+++ Interface/interface.py
@@ -74,7 +74,8 @@ def get_tcp_clients():
     print(tcp_clients_list[1].get('register_group'))
     print(tcp_clients_list[2].get('client'))
     print(tcp_clients_list[2].get('register_group'))
-
+    print(tcp_clients_list[3].get('client'))
+    print(tcp_clients_list[3].get('register_group'))
 
     
 
