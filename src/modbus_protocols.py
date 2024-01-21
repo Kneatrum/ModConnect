81806b59c18d78e0c2dfa846915ae9a9155eee49
Kneatrum <martin.mwiti.n@gmail.com>
2024-01-21 16:03:56 +0300

    Temporary

--- src/modbus_protocols.py
+++ src/modbus_protocols.py
@@ -2,3 +2,41 @@
 This is an abstract class from which we will inherit when implementing
 methods used for connecting to modbus protocols.
 """
+
+
+class ModbusProtocol:
+    def __init__(self, client):
+        self.client = client
+
+
+
+    def connect_to_client(self):
+        if not self.is_connected():
+            self.client.connect()
+    
+
+    def disconnect_from_client(self):
+        self.client.close()
+
+
+    def is_connected(self):
+        if self.client.is_socket_open():
+            return True
+        return False
+
+    
+
+
+
+class ModbusRTU(ModbusProtocol):
+    def __init__(self, client, unit=None):
+        super().__init__(client)
+        if unit is not None:
+            self.unit = unit
+        
+        
+class ModbusTCP(ModbusProtocol):
+    def __init__(self, client, unit=None):
+        super().__init__(self, client)
+        if unit is not None:
+            self.unit = unit
