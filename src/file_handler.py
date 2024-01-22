eb422720d8c31aeb72405bca312c0c28738545d0
Kneatrum <martin.mwiti.n@gmail.com>
2024-01-22 18:15:38 +0300

    Updates to file_handler module - Moved file path inside FileHandler class - get_register_addresses method now renamed to get_register_names_and_addresses. - get_register_names_and_addresses now returning a dictionary with a list of register name and address. - Commented out prototypes of add_device() and save_register_data()

--- src/file_handler.py
+++ src/file_handler.py
@@ -7,6 +7,10 @@ import os
 import json
 import re
 
+
+
+
+
 # Variable to store the path to our json file which we use to store register data.
 FILE_PATH = ''
 
@@ -18,28 +22,30 @@ REGISTER_PREFIX = 'register_'
 
 # Constants storing some of the key strings that make up the json file
 REGISTERS = 'registers'
+REGISTER_NAME = 'register_name'
 REGISTER_ADDRESS = 'address'
 CONNECTION_PARAMETERS = 'connection_params'
 TCP_PARAMETERS = 'tcp'
 RTU_PARAMETERS = 'rtu'
 
 
-# Set the default path for a windows system.
-if os.name == 'nt':
-    FILE_PATH = os.path.join(os.getcwd(), 'data', 'register_map_file.json')
-
-
-# Set the default path for a linux system.
-if os.name == 'posix':
-    # TODO: Enter path for linux system.
-    pass
-
-
 
 
 class FileHandler:
-    def __init__(self, path):
-        self.file_path = path
+
+    # Set the default path for a windows system.
+    if os.name == 'nt':
+        FILE_PATH = os.path.join(os.getcwd(), 'data', 'register_map_file.json')
+
+
+    # Set the default path for a linux system.
+    if os.name == 'posix':
+        # TODO: Enter path for linux system.
+        pass
+
+
+    def __init__(self):
+        self.file_path = self.FILE_PATH
         self.directory = os.path.dirname(self.file_path)
 
 
@@ -136,8 +142,13 @@ class FileHandler:
         return count
 
 
-
-    def get_register_addresses(self, device_number) -> dict:
+    """
+    This method returns a register_x dictionary containing a list of the 
+    register name and the address.
+    Example:
+    register_1: [register_name, register_address]
+    """
+    def get_register_names_and_addresses(self, device_number) -> dict:
         if not self.data_path_exists():
             print("Data path not found")
             return None
@@ -152,7 +163,7 @@ class FileHandler:
                 # We may not need to use a dictionary here. Maybe a list of the addresses is enough.
                 result = dict()
                 for reg in data[device][REGISTERS]:
-                    result[reg] = data[device][REGISTERS][reg][REGISTER_ADDRESS]
+                    result[reg] = list(data[device][REGISTERS][reg][REGISTER_NAME], data[device][REGISTERS][reg][REGISTER_ADDRESS])
         return result
 
 
@@ -193,11 +204,48 @@ class FileHandler:
 
 
     def update_register_name(self, device_number, register_address) -> bool:
+    # update_register_name(device_id:int, row:int, name:str):
+    # target_register = "register_" + str(row+1)
+    # # Read the register setup file
+    # data = read_register_setup_file()
+    # data["device_" + str(device_id)]['registers'][target_register].update({'Register_name':name})
+    # with open(path_to_register_setup, 'w') as f:
+    #     json.dump(data, f, indent=4)
         pass
 
 
-    def add_device(self, device_number, device_params) -> bool:
+
+
+    def add_device(self, user_input) -> bool:
+        # print(user_input)
+        # Get the quantity of registers to poll from. 
+        # reg_quantity = user_input_dict["quantity"]
+        # device_id = user_input_dict["device"]
+        # device = "device_" + str(device_id)
+        # unit_id = str(user_input_dict["slave_address"])
+        # with open(path_to_register_setup, 'w') as f:
+        #     parent_data = {}
+        #     register_data = {}
+        #     # Loop through the list of registers entered by the user
+        #     for i in range(int(reg_quantity)):
+        #         parent_key = "register_" + str(i+1)  # This is the initial  name assigned to the variable that will be read from the register. The user will be allowed to rename the register later. 
+        #         parent_value = {} 
+        #         # Assigning values to all the registr attributes
+        #         # parent_value["address"] = int(user_input_dict["registers"]["address"]) + i # If the user wants to read say 10 registers after register 1000, this line of code increments the addresses to register number 1011
+        #         # parent_value["Register_name"] = user_input_dict["registers"]["Register_name"] 
+        #         # parent_value["function_code"] = user_input_dict["registers"]["function_code"]
+        #         # parent_value["Units"] = user_input_dict["registers"]["Units"]
+        #         # parent_value["Gain"] = user_input_dict["registers"]["Gain"]
+        #         # parent_value["Data_type"] = user_input_dict["registers"]["Data_type"]
+        #         # parent_value["Access_type"] = user_input_dict["registers"]["Access_type"]
+        #         # parent_data[parent_key] = parent_value 
+        #     json.dump({device: {'slave_address':unit_id, 'registers':parent_data}},f) # Appenining the register attributes with the json structure
+        #     register_data[parent_key] = parent_value 
+        #     json.dump({device: {'slave_address':unit_id, 'registers':register_data}},f) # Appenining the register attributes with the json structure
+        #     print("JSON file created!")
         pass
+
+
 
 
     def delete_register(self, device_number, register_address) -> bool:
@@ -208,6 +256,31 @@ class FileHandler:
         pass
 
 
-    def __save_register_addresses(self, device_number) -> bool: ######################
+    def save_register_data(self, user_input:dict) -> bool: ######################
+        # device_id = user_input_dict["device"]
+        # reg_quantity = user_input_dict["quantity"]
+        # # Read the register setup file
+        # data = read_register_setup_file()
+        # # Loop through the list of registers entered by the user and update the the register setup file
+        # for i in range(int(reg_quantity)):
+        #     existing_register_count = register_count_under_device(data,device_id) # Find the number of registers that exist
+        #     # print("Existing register count {}".format(existing_register_count))
+        #     new_register_start = "register_" + str(existing_register_count + 1) # If x registers exist, the next register will be x+1
+        #     # print("New register start {}".format(new_register_start))
+        #     parent_value = {} 
+        #     # Assigning values to all the registr attributes
+        #     parent_value["address"] = int(user_input_dict["registers"]["address"]) + i # If the user wants to read say 10 registers after register 1000, this line of code increments the addresses to register number 1011
+        #     parent_value["Register_name"] = user_input_dict["registers"]["Register_name"] 
+        #     parent_value["function_code"] = user_input_dict["registers"]["function_code"]
+        #     parent_value["Units"] = user_input_dict["registers"]["Units"]
+        #     parent_value["Gain"] = user_input_dict["registers"]["Gain"]
+        #     parent_value["Data_type"] = user_input_dict["registers"]["Data_type"]
+        #     parent_value["Access_type"] = user_input_dict["registers"]["Access_type"] 
+
+        #     # Update the existing json file with the new register parameters
+        #     data["device_" + str(device_id)]['registers'].update({new_register_start:parent_value})
+        #     with open(path_to_register_setup, 'w') as f:
+        #         json.dump(data, f, indent=4)
+        # print("JSON file created!")
         pass
 
