d5b212744028d315a9ea991b073e68bbe33296fc
Kneatrum <martin.mwiti.n@gmail.com>
2024-03-16 22:41:51 +0300

    Added a class that will store the different notifications that need to be sent to the user

--- src/notifications.py
+++ src/notifications.py
@@ -2,3 +2,15 @@
 This module contains methods for showing notifications
 to the user using pop up windows.
 """
+
+from PyQt5.QtWidgets import QMessageBox
+
+class Notification:
+      
+    def set_warning_message(self, title, message):
+        self.messageBox = QMessageBox()
+        self.messageBox.setIcon(QMessageBox.Warning)
+        self.messageBox.setWindowTitle(title)
+        self.messageBox.setText(message)
+        self.messageBox.setStandardButtons(QMessageBox.Ok)
+        self.messageBox.exec_()
