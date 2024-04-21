98849df00003f26a180484fc66367e6d67a64099
Kneatrum <martin.mwiti.n@gmail.com>
2024-04-21 04:22:17 +0300

    Adding the spacer item module

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from constants import STATUS, WIDGET, DISCONNECT, CONNECT, SELECT_ACTION_ID, ADD
 
 from PyQt5.QtWidgets import  QScrollArea, QWidget,  QAction, \
     QVBoxLayout, QDialog, QHBoxLayout,  QTableWidget, \
-    QTableWidgetItem,  QToolBar, QCheckBox, QLabel, QSizePolicy
+    QTableWidgetItem,  QToolBar, QCheckBox, QLabel, QSizePolicy, QSpacerItem
 
 from constants import resource_path
 
